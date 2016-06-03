c5238b92692637d6ac4403e1add38f429eb41e1e
yarolegovich <yarolegovich@gmail.com>
2016-06-03 22:06:03 +0300

    basic workspace structure

---
+++ app/src/main/java/com/yarolegovich/motionink/view/SquareSurfaceView.java
@@ -0,0 +1,41 @@
+package com.yarolegovich.motionink.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.Surface;
+import android.view.SurfaceView;
+
+/**
+ * Created by yarolegovich on 03.06.2016.
+ */
+public class SquareSurfaceView extends SurfaceView {
+    public SquareSurfaceView(Context context) {
+        super(context);
+    }
+
+    public SquareSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SquareSurfaceView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        if (width > height) {
+            super.onMeasure(MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY), heightMeasureSpec);
+        } else {
+            super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY));
+        }
+    }
+}
